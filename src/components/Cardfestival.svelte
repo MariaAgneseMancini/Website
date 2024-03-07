9f092f97e388ee71d511066c80e9b41a920fdf6e
MariaAgneseMancini <162460143+MariaAgneseMancini@users.noreply.github.com>
2024-03-07 17:48:51 +0100

    Update Cardfestival.svelte

--- src/components/Cardfestival.svelte
+++ src/components/Cardfestival.svelte
@@ -2,14 +2,15 @@
 	export let titolo;
 	export let descrizione;
 	export let bottone;
+	export let link;
 </script>
 
 <div class="Card_festival">
 	<p class="titolo">{titolo}</p>
 	<p class="descrizione">{descrizione}</p>
-	<div class="Card_bottone">
+	<a class="Card_bottone" href={link}>
 		<p class="bottone">{bottone}</p>
-	</div>
+	</a>
 </div>
 
 <style>
@@ -20,24 +21,33 @@
 		padding: 10px;
 		color: #14ff00;
 		font-family: sans-serif;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 
 	.titolo {
 		font-size: 32 px;
+		text-align: center;
 	}
 
 	.descrizione {
 		font-size: 9px;
+		text-align: center;
 	}
 
 	.Card_bottone {
 		background-color: #14ff00;
 		border-radius: 20px;
 		padding: 10px;
+		margin-top: 10px;
 	}
 
 	.bottone {
 		font-size: 12px;
 		color: white;
+		text-align: center;
 	}
 </style>
